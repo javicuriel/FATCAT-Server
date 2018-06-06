1307be86c5379c07925b14140b1c99c78978aded
javicuriel <javiercuriel94@gmail.com>
2018-06-06 18:39:08 +0200

    Code clean uo

--- routes/historic.js
+++ routes/historic.js
@@ -34,6 +34,7 @@ router.get('/data', function(req, res, next){
                 total_carbon: row.value.data.total_carbon,
                 max_temp: row.value.data.max_temp,
                 date: row.value.data.timestamp+'Z',
+                baseline: row.value.data.baseline,
                 timestamp: new Date(row.value.data.timestamp+'Z').getTime()
               });
             });
@@ -56,52 +57,6 @@ router.get('/data', function(req, res, next){
     res.send("Invalid Date");
   }
 });
-
-// router.get('/data', function(req, res, next){
-//   data = {"cols":[{"id":"dt","label":"Datetime","type":"date"},{"id":"tc","label":"Total Carbon (ug)","type":"number"},{"id":"tempoven","label":"Temperature (degC)","type":"number"},{"id":"co2base","label":"CO2 baseline (ppm)","type":"number"},{"id":"year","label":"Year","type":"number"},{"id":"month","label":"Month","type":"number"},{"id":"day","label":"Day","type":"number"},{"id":"timeofday","label":"Time","type":"timeofday"},{"id":"runtime","label":"Instrument runtime (s)","type":"number"},{"id":"id","label":"Event ID","type":"string"},{"id":"link","label":"Event Detail","type":"string"}],
-//   rows:[]
-//   };
-//   rows = [];
-//   if(validate_date(req.query.from) && validate_date(req.query.to)){
-//     databases = getDatabasebyDates(req.query.from, req.query.to);
-//     total_rows = databases.length;
-//     databases.forEach(function(db){
-//       view ='/_design/iotp/_view/by-eventType?key="analysis"'
-//       url = credentials.url + '/' + db + view;
-//       https.get(url, function(http_res) {
-//         var body = new Buffer(0);
-//         if (http_res.statusCode == 200){
-//           http_res.on('data', function(chunk) {
-//             body = Buffer.concat( [ body, chunk ] );
-//           });
-//           http_res.on('end',function(){
-//             JSON.parse(body).rows.forEach(function (row) {
-//               row.timestamp = new Date(row.value.data.timestamp+'Z').getTime();
-//               rows.push(row);
-//             });
-//             --total_rows;
-//             if(total_rows <= 0){
-//               res.setHeader('Content-Type', 'application/json');
-//               rows.sort(function(a, b){return b.timestamp - a.timestamp});
-//               rows.forEach(function(row){
-//                 row = format_row(row);
-//                 data.rows.push(row);
-//               });
-//               res.send(data);
-//             }
-//           });
-//         }
-//         else{
-//           --total_rows;
-//           console.log(http_res.statusCode);
-//         }
-//       });
-//     });
-//   }
-//   else{
-//     res.send("Invalid Date");
-//   }
-// });
 
 function format_row(row){
   r = {c:[]};
@@ -129,20 +84,18 @@ function getDatabasebyDates(startDate, stopDate) {
     var currentDate = moment(startDate);
     var stopDate = moment(stopDate);
     while (currentDate <= stopDate) {
-        dateArray.push(base + moment(currentDate).format('YYYY-MM-DD') )
-        currentDate = moment(currentDate).add(1, 'days');
-        // dateArray.push(base + moment(currentDate).format('YYYY-MM') )
-        // currentDate = moment(currentDate).add(1, 'months');
+        dateArray.push(base + moment(currentDate).format('YYYY-MM') )
+        currentDate = moment(currentDate).add(1, 'months');
     }
     return dateArray;
 }
 
 
 function validate_date(date){
-  min = moment('2018-03-30');
+  min = moment('2018-06-01');
   max = moment();
   d = moment(date, moment.ISO_8601);
-  if (!d.isValid() || date < min || date > max){
+  if (!d.isValid() || d < min || d > max){
     return false;
   }
   else{
