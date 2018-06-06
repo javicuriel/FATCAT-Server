f037aee771f1170dc2ab9f16cc7937b0f00ba6ad
javicuriel <javiercuriel94@gmail.com>
2018-06-06 23:04:19 +0200

    Historic retouches

--- routes/historic.js
+++ routes/historic.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var credentials = require('../dbcredentials');
+var credentials = require('../config/database').credentials;
+var cloud = require('../config/cloud');
 var https = require('https');
 var moment = require('moment');
 
@@ -18,6 +19,10 @@ router.get('/data', function(req, res, next){
   data = {rows:[]};
   if(validate_date(req.query.from) && validate_date(req.query.to)){
     databases = getDatabasebyDates(req.query.from, req.query.to);
+    if(databases.length == 0){
+      res.send(data);
+      return;
+    }
     total_rows = databases.length;
     databases.forEach(function(db){
       view ='/_design/iotp/_view/by-eventType?key="analysis"'
@@ -54,48 +59,32 @@ router.get('/data', function(req, res, next){
     });
   }
   else{
-    res.send("Invalid Date");
+    res.send(data);
   }
 });
 
-function format_row(row){
-  r = {c:[]};
-  date = new Date(row.timestamp);
-  date_string = "Date("+date.getFullYear()+", "+date.getMonth()+", "+date.getDay()+", "+date.getHours()+", "+date.getMinutes()+", "+date.getSeconds()+")"
-  r.c.push({v:date_string, f:moment(date).format('DD.MM.YYYY h:mm:ss')});
-  r.c.push({v:row.value.data.total_carbon});
-  r.c.push({v:row.value.data.max_temp});
-  r.c.push({v:1.5});
-  r.c.push({v:date.getFullYear()});
-  r.c.push({v:date.getMonth()});
-  r.c.push({v:date.getDay()});
-  day = date.getHours()+" "+date.getMinutes()+" "+date.getSeconds();
-  r.c.push({v:day});
-  r.c.push({v:0});
-  r.c.push({v:row.value._id});
-  r.c.push({v:'<a href="#">Link</a>'});
-  return r;
-}
-
-// Using moment
-function getDatabasebyDates(startDate, stopDate) {
-    var base = 'iotp_kbld7d_default_';
+function getDatabasebyDates(start, end) {
+    var base = 'iotp_'+cloud.config.org+'_default_';
     var dateArray = [];
-    var currentDate = moment(startDate);
-    var stopDate = moment(stopDate);
-    while (currentDate <= stopDate) {
-        dateArray.push(base + moment(currentDate).format('YYYY-MM') )
+    var currentDate = moment(start);
+    var endDate = moment(end);
+
+    var min = moment('2018-06-01')
+
+    while (currentDate <= endDate || currentDate.isSame(end, 'month')) {
+        if(currentDate >= min){
+          dateArray.push(base + moment(currentDate).format('YYYY-MM') )
+        }
         currentDate = moment(currentDate).add(1, 'months');
     }
+    
     return dateArray;
 }
 
 
 function validate_date(date){
-  min = moment('2018-06-01');
-  max = moment();
   d = moment(date, moment.ISO_8601);
-  if (!d.isValid() || d < min || d > max){
+  if (!d.isValid()){
     return false;
   }
   else{
