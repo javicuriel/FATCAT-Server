4a8a374148d3a58fb80d316b9874158d1813ba73
javicuriel <javiercuriel94@gmail.com>
2018-06-06 15:25:12 +0200

    Clean up

--- routes/historic.js
+++ routes/historic.js
@@ -10,23 +10,12 @@ router.get('/', function(req, res, next) {
   res.render('historic/index', { title: 'Historic Dashboard', currentUser: {id: req.user.id, username: req.user.username} });
 });
 
-router.get('/dc', function(req, res, next) {
-  res.render('historic/dc', { title: 'DC Dashboard', currentUser: {id: req.user.id, username: req.user.username} });
-});
-
-router.get('/smoothie', function(req, res, next) {
-  res.render('historic/smoothie', { title: 'SmoothieChart', currentUser: {id: req.user.id, username: req.user.username} });
-});
-
 router.get('/fatcat', function(req, res, next){
   res.sendFile('test_data.json', {root: './temp'});
 });
 
 router.get('/data', function(req, res, next){
-  data = {"cols":[{"id":"dt","label":"Datetime","type":"date"},{"id":"tc","label":"Total Carbon (ug)","type":"number"},{"id":"tempoven","label":"Temperature (degC)","type":"number"},{"id":"co2base","label":"CO2 baseline (ppm)","type":"number"},{"id":"year","label":"Year","type":"number"},{"id":"month","label":"Month","type":"number"},{"id":"day","label":"Day","type":"number"},{"id":"timeofday","label":"Time","type":"timeofday"},{"id":"runtime","label":"Instrument runtime (s)","type":"number"},{"id":"id","label":"Event ID","type":"string"},{"id":"link","label":"Event Detail","type":"string"}],
-  rows:[]
-  };
-  rows = [];
+  data = {rows:[]};
   if(validate_date(req.query.from) && validate_date(req.query.to)){
     databases = getDatabasebyDates(req.query.from, req.query.to);
     total_rows = databases.length;
@@ -41,25 +30,24 @@ router.get('/data', function(req, res, next){
           });
           http_res.on('end',function(){
             JSON.parse(body).rows.forEach(function (row) {
-              row.timestamp = new Date(row.value.data.timestamp+'Z').getTime();
-              rows.push(row);
+              data.rows.push({
+                total_carbon: row.value.data.total_carbon,
+                max_temp: row.value.data.max_temp,
+                date: row.value.data.timestamp+'Z',
+                timestamp: new Date(row.value.data.timestamp+'Z').getTime()
+              });
             });
             --total_rows;
             if(total_rows <= 0){
               res.setHeader('Content-Type', 'application/json');
-              rows.sort(function(a, b){return b.timestamp - a.timestamp});
-              rows.forEach(function(row){
-                console.log(row.timestamp);
-                row = format_row(row);
-                data.rows.push(row);
-              });
-
+              data.rows.sort(function(a, b){return a.timestamp - b.timestamp});
               res.send(data);
             }
           });
         }
         else{
-          console.log("Error" + http_res.statusCode);
+          --total_rows;
+          console.log(http_res.statusCode);
         }
       });
     });
@@ -68,6 +56,52 @@ router.get('/data', function(req, res, next){
     res.send("Invalid Date");
   }
 });
+
+// router.get('/data', function(req, res, next){
+//   data = {"cols":[{"id":"dt","label":"Datetime","type":"date"},{"id":"tc","label":"Total Carbon (ug)","type":"number"},{"id":"tempoven","label":"Temperature (degC)","type":"number"},{"id":"co2base","label":"CO2 baseline (ppm)","type":"number"},{"id":"year","label":"Year","type":"number"},{"id":"month","label":"Month","type":"number"},{"id":"day","label":"Day","type":"number"},{"id":"timeofday","label":"Time","type":"timeofday"},{"id":"runtime","label":"Instrument runtime (s)","type":"number"},{"id":"id","label":"Event ID","type":"string"},{"id":"link","label":"Event Detail","type":"string"}],
+//   rows:[]
+//   };
+//   rows = [];
+//   if(validate_date(req.query.from) && validate_date(req.query.to)){
+//     databases = getDatabasebyDates(req.query.from, req.query.to);
+//     total_rows = databases.length;
+//     databases.forEach(function(db){
+//       view ='/_design/iotp/_view/by-eventType?key="analysis"'
+//       url = credentials.url + '/' + db + view;
+//       https.get(url, function(http_res) {
+//         var body = new Buffer(0);
+//         if (http_res.statusCode == 200){
+//           http_res.on('data', function(chunk) {
+//             body = Buffer.concat( [ body, chunk ] );
+//           });
+//           http_res.on('end',function(){
+//             JSON.parse(body).rows.forEach(function (row) {
+//               row.timestamp = new Date(row.value.data.timestamp+'Z').getTime();
+//               rows.push(row);
+//             });
+//             --total_rows;
+//             if(total_rows <= 0){
+//               res.setHeader('Content-Type', 'application/json');
+//               rows.sort(function(a, b){return b.timestamp - a.timestamp});
+//               rows.forEach(function(row){
+//                 row = format_row(row);
+//                 data.rows.push(row);
+//               });
+//               res.send(data);
+//             }
+//           });
+//         }
+//         else{
+//           --total_rows;
+//           console.log(http_res.statusCode);
+//         }
+//       });
+//     });
+//   }
+//   else{
+//     res.send("Invalid Date");
+//   }
+// });
 
 function format_row(row){
   r = {c:[]};
@@ -97,6 +131,8 @@ function getDatabasebyDates(startDate, stopDate) {
     while (currentDate <= stopDate) {
         dateArray.push(base + moment(currentDate).format('YYYY-MM-DD') )
         currentDate = moment(currentDate).add(1, 'days');
+        // dateArray.push(base + moment(currentDate).format('YYYY-MM') )
+        // currentDate = moment(currentDate).add(1, 'months');
     }
     return dateArray;
 }
