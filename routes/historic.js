d484d171b3b11fb130deaeb8ac7c1a27c8a63324
javicuriel <javiercuriel94@gmail.com>
2018-06-12 13:32:04 +0200

    Historic using analyisis

--- routes/historic.js
+++ routes/historic.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var cloud = require('../config/cloud');
 var moment = require('moment');
 var api = require('../utilities/api');
-
+var analysis_db = require('../config/database').get('carbonmeasurementapp_analysis');
 
 
 /* GET home page. */
@@ -18,34 +18,21 @@ router.get('/fatcat', function(req, res, next){
 router.get('/data', function(req, res, next){
   data = {rows:[]};
   if(validate_date(req.query.from) && validate_date(req.query.to)){
-    databases = getDatabasebyDates(req.query.from, req.query.to);
-    if(databases.length == 0){
-      res.send(data);
-      return;
-    }
-    total_rows = databases.length;
-    view ='/_design/iotp/_view/by-eventType?key="analysis"'
-    databases.forEach(function(db){
-      api.postBody(db+'/_find', null, 'analysis' ,req.query.from, req.query.to , function(status, body){
-        if (status == 200){
-          body.docs.forEach(function (row) {
-            new_row = row.data;
-            new_row.timestamp = new Date(row.data.timestamp+'Z').getTime()
-            new_row.deviceId = row.deviceId;
-            data.rows.push(new_row);
-          });
-          --total_rows;
-          if(total_rows <= 0){
-            res.setHeader('Content-Type', 'application/json');
-            data.rows.sort(function(a, b){return a.timestamp - b.timestamp});
-            res.send(data);
-          }
+    var query = {
+      selector: {
+        timestamp: {
+          $gte: req.query.from,
+          $lt: moment(req.query.to).endOf('day').utc().toString()
         }
-        else{
-          --total_rows;
-          console.log(status);
-        }
+      }
+    };
+    analysis_db.find(query, function(err, d){
+      data.rows = d.docs;
+      data.rows.forEach(function(datum){
+        datum.timestamp = new Date(datum.timestamp).getTime()
       });
+      data.rows.sort(function(a, b){return a.timestamp - b.timestamp});
+      res.send(data);
     });
   }
   else{
@@ -53,23 +40,6 @@ router.get('/data', function(req, res, next){
   }
 });
 
-function getDatabasebyDates(start, end) {
-    var base = 'iotp_'+cloud.config.org+'_default_';
-    var dateArray = [];
-    var currentDate = moment(start);
-    var endDate = moment(end);
-
-    var min = moment('2018-06-01')
-
-    while (currentDate <= endDate || currentDate.isSame(end, 'month')) {
-        if(currentDate >= min){
-          dateArray.push(base + moment(currentDate).format('YYYY-MM') )
-        }
-        currentDate = moment(currentDate).add(1, 'months');
-    }
-
-    return dateArray;
-}
 
 
 function validate_date(date){
