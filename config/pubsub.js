5a0e2bd2874899a2bc2715098a4c1009fbeb40a6
javicuriel <javiercuriel94@gmail.com>
2018-06-20 16:35:26 +0200

    Added job management

--- config/pubsub.js
+++ config/pubsub.js
@@ -1,7 +1,9 @@
 var ibmiot = require("ibmiotf");
 var cloud_settings = require('./cloud');
 var pubsub = new ibmiot.IotfApplication(cloud_settings.config);
-var analysis_db = require('./database').get('carbonmeasurementapp_analysis');
+var database = require('./database')
+var analysis_db = database.get('carbonmeasurementapp_analysis');
+var api = require('../utilities/api');
 
 module.exports = function(socket_io, instruments){
 
@@ -10,6 +12,8 @@ module.exports = function(socket_io, instruments){
   pubsub.on("connect", function () {
     console.log("IOT connected");
     pubsub.subscribeToDeviceStatus("instrument");
+    pubsub.subscribeToDeviceEvents("instrument","+","analysis","json");
+    pubsub.subscribeToDeviceEvents("instrument","+","job","json");
     sockets.reload.emit('reload');
   });
 
@@ -23,8 +27,12 @@ module.exports = function(socket_io, instruments){
       case 'jobs':
         sockets.jobs.to(deviceId).emit('all', event);
         break;
+      case 'job':
+        update_job_status(deviceId, event);
+        break;
       case 'analysis':
         event['deviceId'] = deviceId;
+        analysis_db = database.get('carbonmeasurementapp_analysis');
         analysis_db.insert(event, function(err, body, header) {
           if (err) {
             console.log(err);
@@ -52,3 +60,35 @@ module.exports = function(socket_io, instruments){
 
   return pubsub;
 };
+
+  function delete_job(deviceId, jobId){
+  jobs_db = database.get('carbonmeasurementapp_jobs');
+  api.findJob(jobs_db, deviceId, jobId, function(e, db_job){
+    if(!db_job) return console.log(e);
+    jobs_db.destroy(db_job._id, db_job._rev, function(error, res) {
+      if(error) return console.log(error);
+    });
+  });
+}
+
+function update_job_status(deviceId, job_event){
+  if(job_event.action == 'delete') return delete_job(deviceId, job_event.job.jobId);
+
+  jobs_db = database.get('carbonmeasurementapp_jobs');
+  api.findJob(jobs_db, deviceId, job_event.job.jobId, function(e, db_job){
+    if(!db_job) return console.log("Error");
+    switch (job_event.action) {
+      case 'add':
+        db_job.job = job_event.job;
+        db_job.job.status = 'active';
+        if(db_job.old_job) delete db_job.old_job;
+        break;
+      case 'disable':
+        db_job.job.status = 'disabled';
+        break;
+    }
+    jobs_db.insert(db_job, function(err, body, header) {
+      if (err) return res.send(err.statusCode);
+    });
+  });
+}
