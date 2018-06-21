ad6aad8d0de5fc512bdbe5283d7d7e0bd672efbe
javicuriel <javiercuriel94@gmail.com>
2018-06-21 20:00:06 +0200

    Documentation database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+// Gets Database credentials with VCAP_SERVICES (Production) or via local file (development)
 function getDBCredentialsUrl(jsonData) {
     var vcapServices = JSON.parse(jsonData);
     // Pattern match to find the first instance of a Cloudant service in
@@ -24,6 +25,7 @@ var Cloudant = require('@cloudant/cloudant');
 
 var cloudant = Cloudant(credentials);
 
+// Exports db credentias and function to get db by name
 module.exports = {
   credentials,
   get: function(database_name){
