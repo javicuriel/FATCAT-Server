41fca4a9efee8bce8350e598540df3a849db00e1
javicuriel <javiercuriel94@gmail.com>
2018-06-21 19:58:24 +0200

    Documentation cloud.js

--- config/cloud.js
+++ config/cloud.js
@@ -1,3 +1,4 @@
+// Sets up IOT server configuration from vcapServices or enviroment 
 if (process.env.VCAP_SERVICES) {
   var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
   var config = vcapServices['iotf-service'][0].credentials;
