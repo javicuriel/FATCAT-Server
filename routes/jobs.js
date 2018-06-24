7e1ed831a5ac6042617231622682e8d5f4b5f855
javicuriel <javiercuriel94@gmail.com>
2018-06-24 19:05:15 +0200

    Added refresh state for pending jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -79,6 +79,7 @@ router.post('/:id/disable', function(req, res, next){
     if(db_job){
       if (db_job.status == 'scheduled'){
         db_job.status = 'pending disable';
+        db_job.mqtt_message = {'action':'disable'};
         jobs_db.insert(db_job, function(err, body, header) {
           if (err) return res.send(err.statusCode);
           event = {'action':'disable','job': {'_id': db_job._id}}
@@ -98,6 +99,7 @@ router.post('/:id/enable', function(req, res, next){
     if(db_job){
       if (db_job.status == 'disabled'){
         db_job.status = 'pending enable';
+        db_job.mqtt_message = {'action':'add'};
         jobs_db.insert(db_job, function(err, body, header) {
           if (err) return res.send(err.statusCode);
           req.pubsub.publishDeviceCommand("instrument", db_job.deviceId, 'job', "txt", {'action':'add','job': db_job}, 1);
@@ -125,6 +127,7 @@ router.post('/:id/delete', function(req, res, next){
       }
       else if (db_job.status == 'scheduled') {
         db_job.status = 'pending delete';
+        db_job.mqtt_message = {'action':'delete'};
         jobs_db.insert(db_job, function(err, body, header) {
           if (err) return res.send(err.statusCode);
           event = {'action':'delete','job': {'_id': db_job._id}}
@@ -136,33 +139,64 @@ router.post('/:id/delete', function(req, res, next){
   });
 });
 
+router.post('/:id/refreshState', function(req, res, next){
+  jobs_db.get(req.params.id, function(err, db_job){
+    if(db_job && db_job.mqtt_message){
+      message = db_job.mqtt_message
+      if(message.action == 'add'){
+        message.job = db_job;
+      }
+      else{
+        message.job = {'_id': db_job._id};
+      }
+      req.pubsub.publishDeviceCommand("instrument", db_job.deviceId, 'job', "txt", message, 1);
+      res.send(200);
+    }
+  });
+});
+
 router.post('/:id/edit', function(req, res, next){
   var edit_job = format_validate_job(req.body, true);
   jobs_db.get(req.params.id, function(err, db_job){
     if(db_job){
-      if(!(db_job.status == 'scheduled' || db_job.status == 'disabled')) return res.send("Cannot edit pending jobs");
-      // Add new old_job -> pending delete
-      var old_job = JSON.parse(JSON.stringify(db_job));
-      old_job._id = old_job._id+'_old';
-      old_job.jobId = old_job.jobId+'_old';
-      old_job.status = 'pending delete';
-      delete old_job._rev;
-
+      status = db_job.status.split(' ')[0];
+      if(!(status == 'scheduled' || status == 'disabled')) return res.send("Cannot edit pending jobs");
       // Edit job -> pending substitution
       db_job.trigger = edit_job.trigger;
       db_job.actions = edit_job.actions;
-      db_job.status = 'pending substitution';
 
-      jobs_db.insert(old_job, function(o_err, o_body, o_header) {
-        if(o_err) return res.send(o_err);
-        db_job.old = o_body;
+
+      if(status == 'disabled'){
+        db_job.status = 'pending activation';
+        db_job.mqtt_message = {'action':'add'};
         jobs_db.insert(db_job, function(d_err, d_body, d_header) {
           if(d_err) return res.send(d_err);
-          req.pubsub.publishDeviceCommand("instrument", db_job.deviceId, 'job', "txt", {'action':'add','job': db_job}, 1);
-          // res.send({o_body, d_body});
+          req.pubsub.publishDeviceCommand("instrument", db_job.deviceId, 'job', "txt", {'action':'add', 'job': db_job}, 1);
           res.redirect('back');
         });
-      });
+      }
+      else{
+        db_job.status = 'pending substitution';
+        // Add new old_job -> pending delete
+        var old_job = JSON.parse(JSON.stringify(db_job));
+        old_job._id = old_job._id+'_old';
+        old_job.jobId = old_job.jobId+'_old';
+        old_job.status = 'pending delete';
+        delete old_job._rev;
+        old_job.mqtt_message = {'action':'delete'};
+
+        jobs_db.insert(old_job, function(o_err, o_body, o_header) {
+          if(o_err) return res.send(o_err);
+          db_job.old = o_body;
+          db_job.mqtt_message = {'action':'add'};
+          jobs_db.insert(db_job, function(d_err, d_body, d_header) {
+            if(d_err) return res.send(d_err);
+            req.pubsub.publishDeviceCommand("instrument", db_job.deviceId, 'job', "txt", {'action':'add','job': db_job}, 1);
+            // res.send({o_body, d_body});
+            res.redirect('back');
+          });
+        });
+      }
     }
     else{
       res.send(404);
@@ -179,6 +213,7 @@ router.post('/add', function(req, res, next){
     jobs_db.get(new_job._id, function(e, db_job){
       // Create new job
       if(!db_job){
+        new_job.mqtt_message = {'action':'add'};
         jobs_db.insert(new_job, function(err, body, header) {
           if (err) return res.send(err.statusCode);
           req.pubsub.publishDeviceCommand("instrument", new_job.deviceId, 'job', "txt", {'action':'add','job': new_job}, 1);
